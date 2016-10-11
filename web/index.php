38e7087d47dfed5986bf6d14b929d1515d04672a
Benjamin Eberlei <kontakt@beberlei.de>
2016-10-11 10:38:59 +0200

    Set timezone

--- web/index.php
+++ web/index.php
@@ -5,6 +5,8 @@ require_once __DIR__ . "/../app/AppKernel.php";
 
 use Symfony\Component\HttpFoundation\Request;
 
+date_default_timezone_set('Europe/Berlin');
+
 $request = Request::createFromGlobals();
 $kernel = new AppKernel('dev', true);
 $response = $kernel->handle($request);
